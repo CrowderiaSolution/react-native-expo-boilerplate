a0c554e3294791dffe68edc95f7d75433a877efc
thusitha@crowderia.com <thusitha@crowderia.com>
2017-07-08 17:03:46 +0530

    add view with validation and common text field

--- expo-template/src/screens/addView/addViewScreen.js
+++ expo-template/src/screens/addView/addViewScreen.js
@@ -1,16 +1,70 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Alert } from 'react-native';
+import DateTimePicker from 'react-native-modal-datetime-picker';
+import moment from 'moment';
 
 import { AddViewForm } from './components';
 
 import styles from './styles/addViewScreen';
 
 export default class AddViewScreen extends Component {
+  state = {
+    isDateTimePickerVisible: false,
+    date: moment(),
+  }
+
+  _showDateTimePicker = () => this.setState({ isDateTimePickerVisible: true })
+
+  _handleDatePicked = date => {
+    this.setState({ date });
+    this._handleDateTimePicker();
+  }
+
+  _handleDateTimePicker = () => this.setState({ isDateTimePickerVisible: false })
+
+  _checkTitle() {
+    const { date } = this.state;
+    if (this.state.date > moment()) {
+      return moment(date).format('MMMM Do YYYY, h:mm:ss a');
+    }
+    return 'Pick a Meetup Date';
+  }
+
+  _checkIfButtonDisable() {
+    const { date } = this.state;
+
+    if (date > moment()) {
+      return false;
+    }
+    return true;
+  }
+
+  _create = async values => {
+    Alert.alert(
+      'Data Added!',
+      [
+        { text: 'OK', onPress: () => this.props.navigation.goBack() },
+      ],
+      { cancelable: false }
+    );
+  }
+
   render() {
     return (
       <View style={styles.root}>
-        <AddViewForm />
+        <AddViewForm
+          create={this._create}
+          showDateTimePicker={this._showDateTimePicker}
+          checkTitle={this._checkTitle()}
+        />
+        <DateTimePicker
+          isVisible={this.state.isDateTimePickerVisible}
+          onConfirm={this._handleDatePicked}
+          onCancel={this._handleDateTimePicker}
+          mode="datetime"
+        />
       </View>
     );
   }
 }
+
--- expo-template/src/screens/addView/components/AddViewForm.js
+++ expo-template/src/screens/addView/components/AddViewForm.js
@@ -1,11 +1,59 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { Field, reduxForm } from 'redux-form';
+import { Button } from 'react-native-elements';
+
+import { TextInputWithValidations } from '../../../commons';
+import { addValidations } from '../validations';
+import { Colors } from '../../../../constants/Theme';
 import styles from './styles/AddViewForm';
 
-const AddViewForm = () => (
+const AddViewForm = ({
+  create,
+  showDateTimePicker,
+  checkTitle,
+  handleSubmit,
+  invalid,
+  submiting,
+}) => (
   <View style={styles.container}>
-    <Text>Add View</Text>
+    <Field
+      component={TextInputWithValidations}
+      name="title"
+      label="Title"
+      selectionColor={Colors.greenColor}
+      containerStyle={styles.item}
+    />
+    <Field
+      component={TextInputWithValidations}
+      name="description"
+      label="Description"
+      selectionColor={Colors.greenColor}
+      containerStyle={styles.item}
+      multiline
+    />
+    <View style={styles.item}>
+      <Button
+        raised
+        fontFamily="opnsen"
+        onPress={showDateTimePicker}
+        title={checkTitle}
+      />
+    </View>
+    <View style={styles.buttonCreate}>
+      <Button
+        backgroundColor={Colors.greenColor}
+        title="Create Meetup"
+        raised
+        fontFamily="opnsen"
+        disabled={invalid || submiting}
+        onPress={handleSubmit(create)}
+      />
+    </View>
   </View>
 );
 
-export default AddViewForm;
+export default reduxForm({
+  form: 'addFormView',
+  validate: addValidations,
+})(AddViewForm);
